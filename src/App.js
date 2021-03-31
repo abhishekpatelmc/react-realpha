00578cf97b22c55b3362085fb52f627c20654eda
abhishekpatelmc <mcabhishekpatel2017@gmail.com>
2021-03-31 13:56:55 +0530

    added bootstrap and made responsive

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Sinup from "./components/Signup";
 
 function App() {
   return (
-    <div className="App">
+    <div className="content">
       <Sinup />
     </div>
   );
--- src/components/Signup.js
+++ src/components/Signup.js
@@ -9,35 +9,75 @@ const Sinup = () => {
       <div className="head">
         <h3>Sign Up for ReAlpa</h3>
       </div>
-      <form className="flex">
-        <div className="fl-name">
-          <input type="text" placeholder="First name" />
-          <input type="text" placeholder="Last name" />
-        </div>
-        <div className="other">
-          <input type="email" placeholder="Email" />
-          <input type="password" placeholder="Password" />
-          <input type="password" placeholder="Confirm password" />
 
-          <button type="submit">Sign Up</button>
+      <form className="row g-3">
+        <div className="col-md-6">
+          <input
+            type="text"
+            className="form-control"
+            id="firstName"
+            placeholder="First Name"
+            required
+          />
+        </div>
+        <div className="col-md-6">
+          <input
+            type="text"
+            className="form-control"
+            id="lastName"
+            placeholder="Last Name"
+            required
+          />
+        </div>
+        <div className="col-12">
+          <input
+            type="email"
+            className="form-control"
+            id="email"
+            placeholder="Email"
+            required
+          />
+        </div>
+        <div className="col-12">
+          <input
+            type="password"
+            className="form-control"
+            id="Password"
+            placeholder="password"
+            required
+          />
+        </div>
+        <div className="col-12">
+          <input
+            type="password"
+            className="form-control"
+            id="confirmPassword"
+            placeholder="Confirm Password"
+            required
+          />
+        </div>
+
+        <div className="col-12 d-grid">
+          <button type="submit" className="singup btn btn-primary">
+            Sign in
+          </button>
+        </div>
+
+        <div className="d-grid gap-2">
+          <button className="btn btn-primary social google" type="button">
+            <i className="fab fa-google"></i>
+            Sign in with Google
+          </button>
+          <button className="btn btn-primary social facebook" type="button">
+            <i className="fab fa-facebook-f"></i>
+            Sing in with Facebook
+          </button>
+          <button className="btn btn-primary social linkedin" type="button">
+            <i className="fab fa-linkedin-in"></i>
+            Sign in with Linkedin
+          </button>
         </div>
       </form>
-      <div className="social">
-        <div>
-          <button className="btn" id="google">
-            <i class="fab fa-google"></i>
-            <label>Sign in with Google</label>
-          </button>
-          <button className="btn" id="facebook">
-            <i class="fab fa-facebook-f"></i>
-            <label>Sign in with Facebook </label>
-          </button>
-          <button className="btn" id="linkedin">
-            <i class="fab fa-linkedin-in"></i>
-            <label>Sign in with Linkedin</label>
-          </button>
-        </div>
-      </div>
     </div>
   );
 };
